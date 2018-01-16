d1dc94984d70a8a962bbc2ac510bd86606daab55
Marin Petrunić <marin.petrunic@gmail.com>
2018-01-16 22:10:55 +0100

    adds routing to index page

--- src/index.js
+++ src/index.js
@@ -1,8 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Web3Instance from './web3';
+import {BrowserRouter, Route} from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { AccountList } from './components/account/AccountList';
+import { Contract } from './components/contract/Contract';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const web3 = Web3Instance.web3;
+
+const RouteWithProps = ({ path, exact, strict, component:Component, location, ...rest }) => (
+    <Route
+        path={path}
+        exact={exact}
+        strict={strict}
+        location={location}
+        render={(props) => <Component {...props} {...rest} />}
+    />
+);
+
+ReactDOM.render(
+    <BrowserRouter>
+        <App>
+            <RouteWithProps exact path="/" component={AccountList} web3={web3}/>
+            <RouteWithProps path="/contract" component={Contract} web3={web3}/>
+        </App>
+    </BrowserRouter>,
+    document.getElementById('root')
+);
 registerServiceWorker();
+
+
