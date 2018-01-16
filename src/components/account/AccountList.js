bb1c2dfcc48b7d981d602ac3c28df55b6365db74
Marin Petrunić <marin.petrunic@gmail.com>
2018-01-16 22:11:24 +0100

    modifies account choosing

--- src/components/account/AccountList.js
+++ src/components/account/AccountList.js
@@ -1,19 +1,26 @@
-import React from 'react'
+import React from 'react';
 
 export class AccountList extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             accounts: [],
-        }
+        };
+        this.setAccount.bind(this);
     }
 
     componentWillMount() {
         const { web3 } = this.props;
-
         web3.eth.getAccounts((error, accounts) => {
             this.setState({ accounts });
         })
+    }
+
+    setAccount(account) {
+        const { web3 } = this.props;
+        console.log("Account chosen:", account);
+        web3.account = account;
+        this.props.history.push('/contract')
     }
 
     render() {
@@ -21,10 +28,11 @@ export class AccountList extends React.Component {
 
         return (
             accounts.map((account, index) => (
-                <button key={index} onClick={() => this.props.onClick(account)}>
+                <button key={index} onClick={() => this.setAccount(account)}>
                     {account}
                 </button>
             ))
         );
     }
+
 }
