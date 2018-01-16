696661a8a8b1afb389e9ac824b718ccdedb1418b
Marin Petrunić <marin.petrunic@gmail.com>
2018-01-16 22:11:05 +0100

    application wrapper

--- src/App.js
+++ src/App.js
@@ -1,42 +1,22 @@
 import React from 'react';
 
 import './App.css';
-import Web3Instance from './web3';
-import {TicTacToe} from "./components/TicTacToe";
-import {AccountList} from "./components/OnBoarding";
 
 export default class App extends React.Component {
+
     constructor(props) {
         super(props);
-
-        this.onAccountChoose = this.onAccountChoose.bind(this);
-
-        this.state = {
-            account: null,
-        }
-    }
-
-    onAccountChoose(account) {
-        this.setState({ account });
     }
 
     render() {
-        const web3 = Web3Instance.web3;
-        const contract = Web3Instance.contract;
 
         return (
             <div>
                 <h1 className="title">Tic-Tac-Toe on Ethereum blockchain!</h1>
 
-                {this.state.account ?
-                    <TicTacToe
-                        web3={web3}
-                        account={this.state.account}
-                        contract={contract}
-                    />
-                    :
-                    <AccountList web3={web3} onClick={this.onAccountChoose} />
-                }
+                <div className="container">
+                    {this.props.children}
+                </div>
             </div>
         );
     }
