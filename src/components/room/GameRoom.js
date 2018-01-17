89e9fa9fa0c2065c8def1299fc5b0ee19c7f7258
Marin Petrunić <marin.petrunic@gmail.com>
2018-01-17 01:09:21 +0100

    implements game loading

--- src/components/room/GameRoom.js
+++ src/components/room/GameRoom.js
@@ -16,23 +16,31 @@ export class GameRoom extends React.Component {
             this.props.history.push('/contract');
             return;
         }
+        let that = this;
         web3.contract.getGamesCount(function(error, result) {
-            console.log(result);
+            console.log("Total number of games:", result.toNumber());
+            let games = [];
+            for (let i = 0; i < result.toNumber(); i++) {
+                web3.contract.games(i, function(error, result) {
+                    if(!error) {
+                        if(result[1].toNumber() === 0) {
+                            games.push({
+                                name: result[0],
+                                id: i
+                            });
+                            that.setState({games: games})
+                        }
+                    }
+                })
+            }
         });
-        // console.log("num", gamesCount);
-        // let games = [];
-        // for (let i = 0; i < gamesCount; i++) {
-        //     games.push(web3.contract.games(i));
-        // }
-        // console.log(games);
-        // this.setState({games: games});
     }
 
     render() {
         const { games } = this.state;
         return (
             games.map((game, index) => (
-                <span>{game.name}</span>
+                <span key={game.id}>{game.name}</span>
             ))
         );
     }
