acb269e76e07b36410530c8c6bba3b834a1b65a3
Marin Petrunić <marin.petrunic@gmail.com>
2018-01-17 13:35:01 +0100

    basic bootstrap layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './App.css';
+import { Col, Grid, Row } from 'react-bootstrap';
 
 export default class App extends React.Component {
 
@@ -9,10 +10,16 @@ export default class App extends React.Component {
         return (
             <div>
                 <h1 className="title">Tic-Tac-Toe on Ethereum blockchain!</h1>
-
-                <div className="container">
-                    {this.props.children}
-                </div>
+                <div style={{paddingTop: 30}} />
+                <Grid>
+                    <Row className="show-grid">
+                        <Col xs={0} md={2} lg={3}/>
+                        <Col xs={12} md={8} lg={6}>
+                            {this.props.children}
+                        </Col>
+                        <Col xs={0} md={2} lg={3}/>
+                    </Row>
+                </Grid>
             </div>
         );
     }
