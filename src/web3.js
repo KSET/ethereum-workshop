98cbfea99a189267e70715ab1478fb1ac03db108
Marin Petrunić <marin.petrunic@gmail.com>
2018-01-17 13:12:15 +0100

    refactores logic to web3

--- src/web3.js
+++ src/web3.js
@@ -1,8 +1,6 @@
 import Web3 from 'web3'
 import abi from "./ABI";
 
-const contractAddress = '0x9741aec2eeebf9e3e6c15d4416a57bfb9927a556';
-
 class Web3Instance {
     constructor() {
         if (typeof window.web3 !== 'undefined') {
@@ -16,12 +14,41 @@ class Web3Instance {
 
 export default new Web3Instance();
 
-export function getListOfGames(contract) {
-
+export function listenOnGames(contract, addGameCallback) {
+    contract.getGamesCount(function (error, result) {
+        console.log('Total number of games:', result.toNumber());
+        for (let i = 0; i < result.toNumber(); i++) {
+            contract.games(i, function (error, result) {
+                if (!error) {
+                    addGameCallback({
+                       id: i,
+                       name: result[0],
+                       status: result[1].toNumber(),
+                       turn: result[2].toNumber()
+                    });
+                } else {
+                    console.log("error while fetching game with id:", i, error);
+                }
+            })
+        }
+    });
+    subscribeToEvent(contract, "GameCreated", function (result) {
+       addGameCallback({
+           id: result.gameId.toNumber(),
+           name: result.name,
+           status: result.status.toNumber(),
+           turn: result.turn.toNumber()
+       })
+    });
 }
 
 export function createGame(contract, name) {
-
+    contract.createGame(name, {value: contract.ENTRY_FEE}, function(error, result) {
+        if (!error) {
+            console.log("Contract transaction send: TransactionHash: "
+                + result + " waiting to be mined...");
+        }
+    });
 }
 
 export function joinGame(contract, gameId) {
@@ -37,6 +64,9 @@ export function setContract(web3, contractAddress) {
     console.log("Setting contract on address:", contractAddress);
     const Contract = web3.eth.contract(abi);
     web3.contract = Contract.at(contractAddress);
+    web3.contract.ENTRY_FEE(function (error, result) {
+        web3.contract.ENTRY_FEE = result.toNumber();
+    });
 }
 
 export function subscribeToEvent(contract, eventName, callback) {
@@ -46,7 +76,8 @@ export function subscribeToEvent(contract, eventName, callback) {
 
 
             if (!error) {
-                callback(log);
+                console.log(eventName, log);
+                callback(log.args);
             }
         }
     );
