81e08a4d7997bca556119d884e8bb1541d1dcabe
Marin Petrunić <marin.petrunic@gmail.com>
2018-01-17 13:12:35 +0100

    using new web3 methods for fetching and creating games

--- src/components/room/GameRoom.js
+++ src/components/room/GameRoom.js
@@ -1,47 +1,72 @@
 import React from 'react';
+import { createGame, listenOnGames } from '../../web3';
 
 export class GameRoom extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            games: []
+            games: [],
+            gameName: ''
         };
+        this.joinGame.bind(this);
+        this.createNewGame.bind(this);
+        this.handleChange.bind(this);
     }
 
     componentWillMount() {
         const {web3} = this.props;
-        console.log("contract", web3.contract);
-        if(!web3.contract) {
+        console.log('contract', web3.contract);
+        if (!web3.contract) {
             this.props.history.push('/contract');
             return;
         }
         let that = this;
-        web3.contract.getGamesCount(function(error, result) {
-            console.log("Total number of games:", result.toNumber());
-            let games = [];
-            for (let i = 0; i < result.toNumber(); i++) {
-                web3.contract.games(i, function(error, result) {
-                    if(!error) {
-                        if(result[1].toNumber() === 0) {
-                            games.push({
-                                name: result[0],
-                                id: i
-                            });
-                            that.setState({games: games})
-                        }
-                    }
-                })
-            }
+        listenOnGames(web3.contract, function(game) {
+           let games = that.state.games;
+           games.push(game);
+           that.setState({games: games});
         });
     }
 
+    joinGame = (id) => {
+        console.log('Joining game with id:', id);
+    };
+
+    createNewGame = (name) => {
+        const {web3} = this.props;
+        console.log('Creating game with name:', name);
+        createGame(web3.contract, name);
+        this.setState({gameName: ''});
+    };
+
+    handleChange = (e) => {
+        this.setState({gameName: e.target.value});
+    };
+
     render() {
-        const { games } = this.state;
+        const {games} = this.state;
         return (
-            games.map((game, index) => (
-                <span key={game.id}>{game.name}</span>
-            ))
+            <table>
+                <tbody>
+                    {
+                        games.map((game, index) => (
+                            <tr key={game.id}>
+                                <td>{game.name}</td>
+                                <td>
+                                    <button onClick={() => this.joinGame(game.id)}>Join</button>
+                                </td>
+                            </tr>
+                        ))
+                    }
+                    <tr>
+                        <td><input type="text" value={this.state.gameName} onChange={this.handleChange}/></td>
+                        <td>
+                            <button onClick={() => this.createNewGame(this.state.gameName)}>Create Game</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         );
     }
 
