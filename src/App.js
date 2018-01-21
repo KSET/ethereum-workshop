248dbdcb8f0f7baca9c286fd021545530f87c089
Marin Petrunić <marin.petrunic@gmail.com>
2018-01-21 18:28:16 +0100

    loader

--- src/App.js
+++ src/App.js
@@ -2,25 +2,52 @@ import React from 'react';
 
 import './App.css';
 import { Col, Grid, Row } from 'react-bootstrap';
+import { LoadingOverlay } from './components/utility/LoadingOverlay';
 
 export default class App extends React.Component {
 
-    render() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: false
+        };
+        this.startLoading = this.startLoading.bind(this);
+        this.stopLoading = this.stopLoading.bind(this);
+    }
 
+    startLoading() {
+        this.setState({isLoading: true});
+    }
+
+    stopLoading() {
+        this.setState({isLoading: false});
+    }
+
+    render() {
+        const{children} = this.props;
+
+        let childrenWithProps = React.Children.map(children, child =>
+            React.cloneElement(child, { startLoading: this.startLoading, stopLoading: this.stopLoading }));
         return (
             <div>
+                <LoadingOverlay
+                    isLoading={this.state.isLoading}
+                    text={'Mining transaction...Please Wait!'}
+                >
+                </LoadingOverlay>
                 <h1 className="title">Tic-Tac-Toe on Ethereum blockchain!</h1>
-                <div style={{paddingTop: 30}} />
+                <div style={{paddingTop: 30}}/>
                 <Grid>
                     <Row className="show-grid">
                         <Col xs={0} md={2} lg={3}/>
                         <Col xs={12} md={8} lg={6}>
-                            {this.props.children}
+                            {childrenWithProps}
                         </Col>
                         <Col xs={0} md={2} lg={3}/>
                     </Row>
                 </Grid>
             </div>
+
         );
     }
 }
---
+++ src/components/utility/LoadingOverlay.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export class LoadingOverlay extends React.Component {
+
+    constructor(props) {
+        super(props);
+        const {isLoading, text} = props;
+        this.state = {
+            isLoading: isLoading,
+            text: text ? text : 'Please Wait!'
+        }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.isLoading !== this.state.isLoading) {
+            this.setState({isLoading: nextProps.isLoading});
+        }
+    }
+
+    render() {
+        return (
+            <div className="loader" hidden={!this.state.isLoading}>
+                <div className="loader-content">
+                    <img src="/loader.gif" width="100" height="100" />
+                    <h2 className="loader-text">{this.state.text}</h2>
+                </div>
+            </div>
+        );
+    }
+}
